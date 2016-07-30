b3ba7234ff0f320743ceb8c010de1a6c94699c0e
Kai Hofstetter <kai.hofstetter@gmx.de>
2016-07-30 13:25:15 +0200

    Configuration refactored

--- src/main/java/net/softwareminds/config/TwitterConfig.java
+++ src/main/java/net/softwareminds/config/TwitterConfig.java
@@ -1,29 +1,10 @@
 package net.softwareminds.config;
 
-
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.Environment;
-import org.springframework.social.UserIdSource;
 import org.springframework.social.config.annotation.SocialConfigurerAdapter;
-import org.springframework.social.connect.ConnectionFactoryLocator;
-import org.springframework.social.connect.UsersConnectionRepository;
-
-import javax.inject.Inject;
 
 @EnableAutoConfiguration
 @Configuration
 public class TwitterConfig extends SocialConfigurerAdapter {
-    @Inject
-    private Environment environment;
-
-    @Override
-    public UserIdSource getUserIdSource() {
-        return null;
-    }
-
-    @Override
-    public UsersConnectionRepository getUsersConnectionRepository(ConnectionFactoryLocator connectionFactoryLocator) {
-        return null;
-    }
 }
