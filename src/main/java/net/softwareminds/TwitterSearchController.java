2f06282ccea8ea3dbdad4d7a109614b1ea2864a1
Kai Hofstetter <kai.hofstetter@gmx.de>
2016-07-30 13:08:48 +0200

    Actual twitter search implemented

--- src/main/java/net/softwareminds/TwitterSearchController.java
+++ src/main/java/net/softwareminds/TwitterSearchController.java
@@ -4,7 +4,7 @@ import org.springframework.core.env.Environment;
 import org.springframework.social.config.annotation.ConnectionFactoryConfigurer;
 import org.springframework.social.connect.Connection;
 import org.springframework.social.connect.ConnectionRepository;
-import org.springframework.social.twitter.api.Twitter;
+import org.springframework.social.twitter.api.*;
 import org.springframework.social.twitter.api.impl.TwitterTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,11 +12,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.inject.Inject;
+import java.util.List;
 
 @Controller
 @RequestMapping("/")
 public class TwitterSearchController {
 
+    @Inject
     private Twitter twitter;
 
     private ConnectionRepository connectionRepository;
@@ -30,8 +32,10 @@ public class TwitterSearchController {
 
     @RequestMapping(method = RequestMethod.GET)
     public String helloTwitter(Model model) {
-        Twitter twitter = new TwitterTemplate(env.getProperty("SPRING_SOCIAL_TWITTER_API"),
-                env.getProperty("SPRING_SOCIAL_TWITTER_APPSECRET"));
+        SearchResults results = twitter.searchOperations().search("@majug");
+
+        List<Tweet> searchResults = results.getTweets();
+        model.addAttribute("searchResults", searchResults);
 
         return "searchResults";
     }
