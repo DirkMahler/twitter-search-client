b6adf7f8a4f8ba0581c36888e9bc2d8adec30c4f
Kai Hofstetter <kai.hofstetter@gmx.de>
2016-07-30 22:14:36 +0200

    Search form added.

--- src/main/java/net/softwareminds/TwitterSearchController.java
+++ src/main/java/net/softwareminds/TwitterSearchController.java
@@ -1,6 +1,7 @@
 package net.softwareminds;
 
 import org.springframework.core.env.Environment;
+import org.springframework.http.MediaType;
 import org.springframework.social.connect.ConnectionRepository;
 import org.springframework.social.twitter.api.SearchResults;
 import org.springframework.social.twitter.api.Tweet;
@@ -9,6 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.inject.Inject;
 import java.util.List;
@@ -22,12 +24,19 @@ public class TwitterSearchController {
 
     @RequestMapping(method = RequestMethod.GET)
     public String helloTwitter(Model model) {
-        SearchResults results = twitter.searchOperations().search("@majug");
+        return "search";
+    }
+
+
+    @RequestMapping(value = "/search", method = RequestMethod.POST, consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+    public String formBookingPost(@RequestParam String searchQuery, Model model) {
+        SearchResults results = twitter.searchOperations().search(searchQuery);
 
         List<Tweet> searchResults = results.getTweets();
         model.addAttribute("searchResults", searchResults);
 
         return "searchResults";
     }
+
 
 }
