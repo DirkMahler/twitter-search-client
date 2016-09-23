2521fe2c433e81b1067c54d7096f92c5350f2efe
KaiHofstetter <kai.hofstetter@gmx.de>
2016-09-23 07:38:12 +0200

    improved toggle configuration

--- src/main/java/net/softwareminds/config/TwitterFeatureToggles.java
+++ src/main/java/net/softwareminds/config/TwitterFeatureToggles.java
@@ -4,6 +4,7 @@ import org.togglz.core.Feature;
 import org.togglz.core.activation.UserRoleActivationStrategy;
 import org.togglz.core.annotation.ActivationParameter;
 import org.togglz.core.annotation.DefaultActivationStrategy;
+import org.togglz.core.annotation.EnabledByDefault;
 import org.togglz.core.annotation.InfoLink;
 import org.togglz.core.annotation.Label;
 import org.togglz.core.annotation.Owner;
@@ -11,17 +12,19 @@ import org.togglz.core.context.FeatureContext;
 
 public enum TwitterFeatureToggles implements Feature {
 
-    @DefaultActivationStrategy(id=UserRoleActivationStrategy.ID,
-            parameters = {@ActivationParameter(name = UserRoleActivationStrategy.PARAM_ROLES_NAME, value = "QA")})
-    @Label("Twitter Recent Searches")
-    @InfoLink("http://jira-user-story")
-    @Owner("Kai")
-    TWITTER_RECENT_SEARCHES,
-
+    @EnabledByDefault
     @Label("Twitter Trends")
     @InfoLink("http://jira-user-story")
     @Owner("Kai")
     TWITTER_TRENDS,
+
+    @EnabledByDefault
+    @DefaultActivationStrategy(id=UserRoleActivationStrategy.ID,
+            parameters = {@ActivationParameter(name = UserRoleActivationStrategy.PARAM_ROLES_NAME, value = "QA")})
+    @Label("Twitter Recent Searches Improvement")
+    @InfoLink("http://jira-user-story")
+    @Owner("Kai")
+    RECENT_SEARCHES_IMPROVEMENT,
 
     @Label("Twitter Profiles")
     @InfoLink("http://jira-profile-story")
