6ecbdb158eda1659672d28e499c6cdf458e076f2
KaiHofstetter <kai.hofstetter@gmx.de>
2016-09-27 09:04:38 +0200

    changed user name

--- src/main/java/net/softwareminds/config/WebSecurityConfig.java
+++ src/main/java/net/softwareminds/config/WebSecurityConfig.java
@@ -35,7 +35,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
         //@formatter:off
         auth.inMemoryAuthentication()
-                .withUser("profile")
+                .withUser("user")
                     .password("password")
                     .roles("USER")
             .and()
